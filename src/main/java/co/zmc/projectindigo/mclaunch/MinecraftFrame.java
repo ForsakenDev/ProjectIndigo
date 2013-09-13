22067fb1a1a7adfabcd418c1790a332f5f0e3632
ZephyrusMC <vanZeben@live.ca>
2013-09-13 00:11:13 -0400

    Final update before release

--- src/main/java/co/zmc/projectindigo/mclaunch/MinecraftFrame.java
+++ src/main/java/co/zmc/projectindigo/mclaunch/MinecraftFrame.java
@@ -111,7 +111,7 @@ public class MinecraftFrame extends JFrame {
             }
         }
 
-//         setIconImage(Toolkit.getDefaultToolkit().createImage(imagePath));
+        // setIconImage(Toolkit.getDefaultToolkit().createImage(imagePath));
         super.setVisible(true);
         setResizable(true);
         addWindowListener(new WindowAdapter() {
@@ -146,8 +146,8 @@ public class MinecraftFrame extends JFrame {
         appletWrap.addParameter("username", user);
         appletWrap.addParameter("sessionid", session);
         appletWrap.addParameter("stand-alone", "true");
-        appletWrap.addParameter("server", ip);
-        appletWrap.addParameter("port", port);
+        // appletWrap.addParameter("server", ip);
+        // appletWrap.addParameter("port", port);
         mcApplet.setStub(appletWrap);
         add(appletWrap);
 
