c33b1185f5ff60fbc88d32d75a28cc7b9c88411c
WinMac32 <seth@viaware.ca>
2013-09-29 02:49:40 -0600

    Move things around a bit

--- src/main/java/co/zmc/projectindigo/mclaunch/MinecraftLauncher.java
+++ src/main/java/co/zmc/projectindigo/mclaunch/MinecraftLauncher.java
@@ -6,21 +6,13 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.security.Permission;
-import java.security.Policy;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 
-import javax.swing.JOptionPane;
-import javax.swing.SwingWorker;
-
-import co.zmc.projectindigo.IndigoLauncher;
 import co.zmc.projectindigo.Main;
 import co.zmc.projectindigo.data.Server;
 import co.zmc.projectindigo.data.log.Logger;
-import co.zmc.projectindigo.security.PolicyManager;
 import co.zmc.projectindigo.utils.Settings;
 import co.zmc.projectindigo.utils.Utils;
 
@@ -163,7 +155,8 @@ public class MinecraftLauncher {
             URLClassLoader cl = new URLClassLoader(urls, MinecraftLauncher.class.getClassLoader());
 
             Logger.logInfo("Loading security class");
-            // loadSecurityManager(basepath, nativesDir);
+            //PolicyManager manager = new PolicyManager();
+            //manager.enforceSecurityManager(basepath, nativesDir);
 
             Logger.logInfo("Loading minecraft class");
 
@@ -181,86 +174,5 @@ public class MinecraftLauncher {
         }
     }
 
-    protected static void loadSecurityManager(String basepath, String nativesDir) {
-        PolicyManager policy = new PolicyManager();
-        policy.copySecurityPolicy();
-
-        policy.addAdditionalPerm("permission java.lang.RuntimePermission \"*\"");
-
-        policy.addAdditionalPerm("permission java.io.FilePermission \"" + new File(basepath).getParentFile().getAbsolutePath().replaceAll("\\\\", "/") + "/-\", \"read, write, delete\"");
-        policy.addAdditionalPerm("permission java.io.FilePermission \"" + nativesDir.replaceAll("\\\\", "/") + "/-\", \"read\"");
-        policy.addAdditionalPerm("permission java.io.FilePermission \"" + System.getProperty("java.io.tmpdir").replaceAll("\\\\", "/") + "-\", \"read, write, delete\"");
-        policy.addAdditionalPerm("permission java.io.FilePermission \"" + System.getProperty("java.io.tmpdir").replaceAll("\\\\", "/") + "\", \"read, write, delete\"");
-        policy.addAdditionalPerm("permission java.io.FilePermission \"" + System.getProperty("java.home").replaceAll("\\\\", "/") + "/-\", \"read\"");
-        policy.addAdditionalPerm("permission java.io.FilePermission \"" + System.getProperty("java.home").replaceAll("\\\\", "/").replaceAll(" ", "%20") + "/-\", \"read\"");
-        policy.addAdditionalPerm("permission java.io.FilePermission \"" + IndigoLauncher.class.getProtectionDomain().getCodeSource().getLocation().getPath().replaceAll("\\\\", "/") + "\", \"read\"");
-
-        policy.writeAdditionalPerms(policy.getPolicyLocation());
-
-        System.out.println("Setting security policy to " + policy.getPolicyLocation());
-        System.setProperty("java.security.policy", policy.getPolicyLocation());
-        Policy.getPolicy().refresh();
-
-        final File[] files = new File(nativesDir).listFiles();
-        SecurityManager manager = new SecurityManager() {
-            @Override
-            public void checkPermission(Permission perm) {
-                try {
-                    super.checkPermission(perm);
-                } catch (SecurityException e) {
-                    if ((perm.getName().toLowerCase().contains(".ttf") || perm.getName().toLowerCase().contains(".ttc")) && perm.getActions().equals("read")) { return; }
-
-                    if (perm.getName().contains("loadLibrary.")) {
-                        System.out.println("LOADLIB: " + perm.getName());
-
-                        String libPath = perm.getName().replaceAll("loadLibrary.", "");
-
-                        File file = new File(libPath);
-
-                        System.out.println("Minecraft is attempting to load native : " + file.getAbsolutePath());
-
-                        for (File nv : files) {
-                            if (file.getAbsolutePath().equals(nv.getAbsolutePath())) {
-                                System.out.println("Native loading permitted.");
-                                return;
-                            }
-                        }
-                    }
-
-                    final Permission fPerm = perm;
-
-                    SwingWorker<Boolean, Void> worker = new SwingWorker<Boolean, Void>() {
-
-                        @Override
-                        public Boolean doInBackground() {
-                            System.out.println("Asking user for perm " + fPerm.toString());
-                            return (JOptionPane.showConfirmDialog(null, "A mod is trying to access something stoopid:\n" + fPerm.toString()
-                                    + "\nDo you want to allow it?\nWarning this could allow the mod to access sensitive info.") == JOptionPane.YES_OPTION);
-                        }
-                    };
-
-                    worker.execute();
-
-                    boolean allowed = false;
-
-                    try {
-                        System.out.println("Waiting for response...");
-                        allowed = (Boolean) worker.get();
-                    } catch (InterruptedException e1) {
-                        e1.printStackTrace();
-                    } catch (ExecutionException e1) {
-                        e1.printStackTrace();
-                    }
-
-                    if (!allowed) {
-                        System.out.println("Not allowing permission.");
-                        throw e;
-                    }
-
-                }
-            }
-        };
-
-        System.setSecurityManager(manager);
-    }
+    
 }
