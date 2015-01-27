a251ff02f99ade7d171a9c1781e0039db61e03f9
vanZeben <ryan@vanZeben.ca>
2015-01-26 18:52:30 -0800

    Update sorting to add in the first server to the list

--- src/main/java/co/forsaken/projectindigo/gui/ServerBasePanel.java
+++ src/main/java/co/forsaken/projectindigo/gui/ServerBasePanel.java
@@ -96,7 +96,7 @@ import co.forsaken.projectindigo.utils.Utils;
               Server s = new Server(t);
               if (s != null && s.online) {
                 s.finishedLoading();
-                if (activeServer == null) {
+                if (activeServer == null && (9 - Integer.parseInt(s.getToken().order)) == 1) {
                   setServer(s);
                 }
                 addServer(s);
